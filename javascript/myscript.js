9d8d7402b529df16138ecc98e3b59acd8488c54b
shubham0809200 <shubham.kr08092000@gmail.com>
2020-08-02 04:18:38 +0530

    second page on progress

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,80 @@
- var n=prompt("Please Enter Your Name");
-alert("Hii there "+ n);
+   var n=prompt("Please Enter Your Name");
+
+    document.getElementById("enterName").innerHTML="Hiii "+n +",";
+
+function popular()
+{
+        document.getElementById('popular').style.display='block';
+        document.getElementById('ring').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        
 
 
- document.getElementById("enterName").innerHTML="Hiii "+n;
+}
 
+
+function ring()
+{
+        document.getElementById('ring').style.display='block';
+        document.getElementById('popular').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        
+
+
+}
+
+
+function necklace()
+{
+        document.getElementById('').style.display='block';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        
+
+
+}
+
+
+function bracelet()
+{
+        document.getElementById('').style.display='block';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        
+
+
+}
+
+
+function earRings()
+{
+        document.getElementById('').style.display='block';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        
+
+
+}
+
+
+function bangles()
+{
+        document.getElementById('').style.display='block';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        document.getElementById('').style.display='none';
+        
+
+
+}
